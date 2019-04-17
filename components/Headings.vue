7c9d592616b46bbaabd24be12d17906807ac8444
Vladimir Pavlov <vovavc@gmail.com>
2019-04-17 15:21:14 +0300

    inital commit

---
+++ components/Headings.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1 class="title">
+      nuxtjs-github-pages-test
+    </h1>
+    <h2 class="subtitle">
+      My delightful Nuxt.js project
+    </h2>
+  </div>
+</template>
+
+<style>
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="container">
+    <div>
+      <logo />
+      <headings />
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import Headings from '~/components/Headings.vue'
+
+export default {
+  components: {
+    Logo,
+    Headings
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
