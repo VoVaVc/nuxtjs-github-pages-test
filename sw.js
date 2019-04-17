ab52c504b6a1853398f913ca89f98c26c852f6ed
Vladimir Pavlov <vovavc@gmail.com>
2019-04-17 17:51:03 +0300

    Updates

--- sw.js
+++ sw.js
@@ -2,20 +2,20 @@ importScripts('/nuxtjs-github-pages-test/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/nuxtjs-github-pages-test/_nuxt/307d5a0497481804c8c3.js",
-    "revision": "2853ef70586e915517d17255c62fb17f"
-  },
-  {
-    "url": "/nuxtjs-github-pages-test/_nuxt/64a00d8c0892962526b3.js",
-    "revision": "218fa7bd3bd97782f712c40c7c0e5bb9"
-  },
-  {
     "url": "/nuxtjs-github-pages-test/_nuxt/6b89a279e8ec9729667e.js",
     "revision": "dcde9f0c35498b0d3cdd1ec52530d593"
   },
   {
+    "url": "/nuxtjs-github-pages-test/_nuxt/b273fd0179c750a0d889.js",
+    "revision": "fac724b81e05edb8bc98e88596d00636"
+  },
+  {
     "url": "/nuxtjs-github-pages-test/_nuxt/ce239af73fd3cff12e80.js",
     "revision": "733889b9cb0af5548b1f79c668623273"
+  },
+  {
+    "url": "/nuxtjs-github-pages-test/_nuxt/d34f8062bcebcd3a64b3.js",
+    "revision": "ef4a295560475fce63623877e58fb910"
   },
   {
     "url": "/nuxtjs-github-pages-test/_nuxt/dd02f9122839358a7984.js",
